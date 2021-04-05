df004f750247b43bf298751c8ce724527351c1f2
mridhulDhar <mdhar006@gmail.com>
2021-04-05 14:45:32 -0400

    base url changed v2

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     
-    baseURL: '',
+    baseURL: 'http://' + process.env.APP_DOMAIN + ':' + process.env.APP_PORT
 });
 
 export default instance;
